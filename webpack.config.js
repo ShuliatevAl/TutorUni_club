301e6fb2e531d776cb86c61dae5dc0fa13f185f0
ShuliatevAl <chemistryalexey@rambler.ru>
2023-04-10 15:07:50 +0300

    webpack almost work

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,75 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
-module.exports = {
+const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const localServer = {
+    port: 3000,
+    open: true,
+};
+
+const config = {
     entry: path.resolve(__dirname, 'src', 'index.js'),
     output: {
         path: path.resolve(__dirname, 'dist'),
         clean: true,
-        filename: 'index.[contenthash].js',
+        filename: '[name].[contenthash].js',
     },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: path.resolve(__dirname, 'src', 'index.html')
-        }),
-    ],
     module: {
         rules: [
             {
+                test: /\.(c|sa|sc)ss$/i,
+                use: [
+                    // Creates `style` nodes from JS strings
+                    MiniCssExtractPlugin.loader,
+
+                    // Translates CSS into CommonJS
+                    "css-loader",
+
+                    // Compiles Sass to CSS
+                    "sass-loader",
+                ],
+            },
+            {
                 test: /\.html$/i,
                 loader: 'html-loader',
+            },
+            {
+                test: /\.woff2?$/i,
+                type: 'asset/resource',
+                generator: {
+                    filename: 'fonts/[name][ext]',
+                },
             }
         ]
-    }
-}
+    },
+    plugins: [
+        new BrowserSyncPlugin({
+            proxy: localServer.path,
+            port: localServer.port,
+            files: [],
+            ghostMode: {
+                clicks: false,
+                location: false,
+                forms: false,
+                scroll: false,
+            },
+            injectChanges: true,
+            logFileChanges: true,
+            logLevel: 'debug',
+            logPrefix: 'wepback',
+            notify: true,
+            reloadDelay: 0,
+        }),
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: path.resolve(__dirname, 'src', 'index.html')
+        }),
+        new MiniCssExtractPlugin({
+            filename: '[name].[contenthash].css',
+        }),
+    ]
+};
+
+module.exports = config;
