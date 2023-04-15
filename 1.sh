af9bfc73cb90f4f053579a23679135a5c45162bc
ShuliatevAl <chemistryalexey@rambler.ru>
2023-04-15 11:34:49 +0300

    test

--- 1.sh
+++ 1.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 git filter-branch -f --env-filter '
-OLD_EMAIL="<intermediat@DESKTOP-K8V2R0F>"
+OLD_EMAIL="intermediat@DESKTOP-K8V2R0F"
 CORRECT_EMAIL="chemistryalexey@rambler.ru"
 if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
 then
