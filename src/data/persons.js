ba2f9a9316fe0ef15d61998f5d7a824fb29efbef
ShuliatevAl <chemistryalexey@rambler.ru>
2023-04-20 17:01:15 +0300

    adaptive fix OOP start

---
+++ src/data/persons.js
@@ -0,0 +1,31 @@
+class Person {
+
+    constructor(name) {
+        this.name = name;
+    }
+
+    constructor(surname) {
+        this.surname = surmane;
+    }
+
+    name;
+    surname;
+    sex;
+    age;
+
+    subject;
+    experience;
+    price;
+    places;
+
+    telegram_link;
+    instagram_link;
+    vk_link;
+    twitter_link;
+
+    description;
+}
+
+function createPerson(name, surname) {
+
+}
